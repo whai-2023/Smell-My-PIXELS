af715a917167f4e106a7ea6da619ac9b5ceb3eac
Sarah Park <sarahypark83@gmail.com>
2023-06-01 23:01:26 +1200

    update navbar

--- client/components/PenColour.tsx
+++ client/components/PenColour.tsx
@@ -1,43 +1,59 @@
 import React, { useState } from 'react'
-import "../main.css"
+import '../main.css'
 
 function Navbar() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false)
 
   const handleOpen = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   const handleMenuOne = () => {
     // do something
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const handleMenuTwo = () => {
     // do something
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
-    <div className="dropdown">
-      <button onClick={handleOpen}>Colours</button>
+    <div className="navbar">
+      <h1>Colours!</h1>
+      <h2>Background Colours</h2>
+      <button className="dropdown" onClick={handleOpen}>
+        Colours
+      </button>
       {open ? (
         <ul className="menu">
           <li className="menu-item">
-            <button onClick={handleMenuOne}>Pen Colours</button>
             <li>
               <button onClick={handleMenuOne}>Pink</button>
               <button onClick={handleMenuOne}>Green</button>
               <button onClick={handleMenuOne}>Black</button>
             </li>
           </li>
+        </ul>
+      ) : null}
+
+      <h2>Pen Colours</h2>
+      <button className="dropdown" onClick={handleOpen}>
+        Colours
+      </button>
+      {open ? (
+        <ul className="menu">
           <li className="menu-item">
-            <button onClick={handleMenuTwo}>Background Colours</button>
+            <li>
+              <button onClick={handleMenuTwo}>Pink</button>
+              <button onClick={handleMenuTwo}>Green</button>
+              <button onClick={handleMenuTwo}>Black</button>
+            </li>
           </li>
         </ul>
       ) : null}
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
